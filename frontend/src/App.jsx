298a60a28c5ee3b13d04bf469e696b87c4860372
sejal jaswal <sejaljaswal@sejals-MacBook-Air-10.local>
2025-03-05 14:59:32 +0530

    done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import ASAPCard from "./ASAPCard";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <ASAPCard />
+    </div>
+  );
+}
+
+export default App;
